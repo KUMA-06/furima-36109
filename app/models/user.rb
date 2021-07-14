414253d142810252b55eabcc27a13a6767ac1fdc
KUMA-06 <hyt_kmkr@icloud.com>
2021-07-14 18:33:59 +0900

    バリデーション追加⇨完成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/.freeze
+NAME_KATAKANA = /\A[ァ-ヴー]+\z/u.freeze
 
-  validates_format_of :password, with: PASSWORD_REGEX
+  validates :password, format:{ with: PASSWORD_REGEX}
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_katakana, presence: true
-  validates :first_katakana, presence: true
+  validates :last_name, presence: true, format: { with: NAME_REGEX} 
+  validates :first_name, presence: true, format: { with: NAME_REGEX} 
+  validates :last_katakana, presence: true, format:{ with: NAME_KATAKANA}
+  validates :first_katakana, presence: true, format:{ with: NAME_KATAKANA}
   validates :birthday, presence: true
 
   # has_many :items
