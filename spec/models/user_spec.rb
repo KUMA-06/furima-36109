5ec11cd5314cc5b3e5d2f048e496eee5c8f21e64
KUMA-06 <hyt_kmkr@icloud.com>
2021-07-14 19:01:37 +0900

    rspec導入〜異常形記述

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,102 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'ユーザー新規登録' do
+    context '新規登録できるとき'
+    it 'nicknameが空では登録できない' do
+      # nicknameが空では登録できないテストコードを記述します
+    end
+
+    it 'nicknameが空では登録できない' do
+      # nicknameが空では登録できないテストコードを記述します
+    end
+
+    it 'nicknameが空では登録できない' do
+      # nicknameが空では登録できないテストコードを記述します
+    end
+
+    it 'nicknameが空では登録できない' do
+      # nicknameが空では登録できないテストコードを記述します
+    end
+    
+    it 'nicknameが空では登録できない' do
+      # nicknameが空では登録できないテストコードを記述します
+    end
+
+    it 'nicknameが空では登録できない' do
+      # nicknameが空では登録できないテストコードを記述します
+    end
+
+    end
+    context '新規登録できないとき'
+      it 'nicknameが空では登録できない' do
+        # nicknameが空では登録できないテストコードを記述します
+      end
+
+      it 'emailが空では登録できない' do
+        # emailが空では登録できないテストコードを記述します
+      end
+    
+      it 'emailが重複していると登録できない' do
+        # emailが空では登録できないテストコードを記述します
+      end
+    
+      it 'emailは＠を含まないと登録できない' do
+        # emailが空では登録できないテストコードを記述します
+      end
+    
+      it 'passwordが空では登録できない' do
+        # emailが空では登録できないテストコードを記述します
+      end
+    
+      it 'passwordが５文字以下では登録できない' do
+        # emailが空では登録できないテストコードを記述します
+      end
+      
+      it 'passwordは半角英数字混合でなければ登録できない' do
+        # emailが空では登録できないテストコードを記述します
+      end
+      
+      it 'passwordがpassword_confirmationと一致しなければ登録できない' do
+        # emailが空では登録できないテストコードを記述します
+      end
+      
+      it 'last_nameが空では登録できない' do
+        # emailが空では登録できないテストコードを記述します
+      end
+      
+      it 'last_nameは全角でなければ登録できない' do
+        # emailが空では登録できないテストコードを記述します
+      end
+
+      it 'first_nameが空では登録できない' do
+        # emailが空では登録できないテストコードを記述します
+      end
+
+      it 'first_nameは全角でなければ登録できない' do
+        # emailが空では登録できないテストコードを記述します
+      end
+
+      it 'last_katakanaが空では登録できない' do
+        # emailが空では登録できないテストコードを記述します
+      end
+
+      it 'last_katakanaは全角カタカナでなければ登録できない' do
+        # emailが空では登録できないテストコードを記述します
+      end
+
+      it 'first_katakanaが空では登録できない' do
+        # emailが空では登録できないテストコードを記述します
+      end
+
+      it 'first_katakanaは全角カタカナでなければ登録できない' do
+        # emailが空では登録できないテストコードを記述します
+      end
+
+      it 'birthdayが空では登録できない' do
+        # emailが空では登録できないテストコードを記述します
+      end
+
+    end
+  end
+end
