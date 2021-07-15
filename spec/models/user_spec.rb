edd967afda6ce7e431f04f0027ebb7f25225b96a
KUMA-06 <hyt_kmkr@icloud.com>
2021-07-15 12:48:50 +0900

    テストコード記述

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,100 +1,125 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
   describe 'ユーザー新規登録' do
-    context '新規登録できるとき'
+    context '新規登録できるとき' do
       it 'nickname,email,password,password_confirmation,last_name,first_name,last_katakana,first_katakana,birthdayが存在すれば登録できる' do
-        # nicknameが空では登録できないテストコードを記述します
+        expect(@user).to be_valid
       end
-
-      it 'passwordが６文字以上かつ英数字混合なら登録できる' do
-        # nicknameが空では登録できないテストコードを記述します
-      end
-
-      it 'passwordとpassword_confirmationが一致すれば登録できる' do
-        # nicknameが空では登録できないテストコードを記述します
-      end
-
-      it 'last_name,first_nameが全角文字なら登録できる' do
-        # nicknameが空では登録できないテストコードを記述します
-      end
-    
-      it 'last_katakana,first_katakanaが全角カタカナなら登録できる' do
-        # nicknameが空では登録できないテストコードを記述します
-      end
-
     end
-    context '新規登録できないとき'
+
+    context '新規登録できないとき' do
       it 'nicknameが空では登録できない' do
-        # nicknameが空では登録できないテストコードを記述します
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
       end
 
       it 'emailが空では登録できない' do
-        # emailが空では登録できないテストコードを記述します
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
       end
     
       it 'emailが重複していると登録できない' do
-        # emailが空では登録できないテストコードを記述します
+        @user.save
+        another_user = FactoryBot.build(:user, email: @user.email)
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include("Email has already been taken")
       end
     
       it 'emailは＠を含まないと登録できない' do
-        # emailが空では登録できないテストコードを記述します
+        @user.email = 'aaaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email is invalid")
       end
     
       it 'passwordが空では登録できない' do
-        # emailが空では登録できないテストコードを記述します
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
       end
     
       it 'passwordが５文字以下では登録できない' do
-        # emailが空では登録できないテストコードを記述します
+        @user.password = '55a55'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
       end
       
       it 'passwordは半角英数字混合でなければ登録できない' do
-        # emailが空では登録できないテストコードを記述します
+        @user.password = '666666'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
       end
       
       it 'password_confirmationが空では登録できない' do
-        # emailが空では登録できないテストコードを記述します
+        @user.password_confirmation = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
 
       it 'passwordがpassword_confirmationと一致しなければ登録できない' do
-        # emailが空では登録できないテストコードを記述します
+        @user.password = 'a12345'
+        @user.password_confirmation = 'a123456'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
       
       it 'last_nameが空では登録できない' do
-        # emailが空では登録できないテストコードを記述します
+        @user.last_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name can't be blank")
       end
       
       it 'last_nameは全角でなければ登録できない' do
-        # emailが空では登録できないテストコードを記述します
+        @user.last_name = 'asdf'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name is invalid")
       end
 
       it 'first_nameが空では登録できない' do
-        # emailが空では登録できないテストコードを記述します
+        @user.first_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name can't be blank")
       end
 
       it 'first_nameは全角でなければ登録できない' do
-        # emailが空では登録できないテストコードを記述します
+        @user.first_name = 'asdf'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name is invalid")
       end
 
       it 'last_katakanaが空では登録できない' do
-        # emailが空では登録できないテストコードを記述します
+        @user.last_katakana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last katakana can't be blank")
       end
 
       it 'last_katakanaは全角カタカナでなければ登録できない' do
-        # emailが空では登録できないテストコードを記述します
+        @user.last_katakana = 'ひらがな'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last katakana is invalid")
       end
 
       it 'first_katakanaが空では登録できない' do
-        # emailが空では登録できないテストコードを記述します
+        @user.first_katakana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First katakana can't be blank")
       end
 
       it 'first_katakanaは全角カタカナでなければ登録できない' do
-        # emailが空では登録できないテストコードを記述します
+        @user.first_katakana = 'ひらがな'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First katakana is invalid")
       end
 
       it 'birthdayが空では登録できない' do
-        # emailが空では登録できないテストコードを記述します
+        @user.birthday = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Birthday can't be blank")
       end
 
     end
