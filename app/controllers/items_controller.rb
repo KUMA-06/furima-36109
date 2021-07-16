f9b5b7657ad1f73e97dc67e8791dbfb4ec97fe26
KUMA-06 <hyt_kmkr@icloud.com>
2021-07-16 15:34:35 +0900

    アクティブハッシュ作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,9 +6,18 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   private
 
-  def message_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+  def item_params
+    params.require(:item).permit(:image, :item_name, :description, :category_id, :condition_id, :charge_id, :area_id, :day_id, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,16 +3,25 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :item_name
     validates :description
-    validates :category_id
-    validates :condition_id
-    validates :charge_id
-    validates :area_id
-    validates :day_id
-    validates :price 
+    validates :price, format: { with: /\A[0-9]+\z/ }, inclusion: { in: (300..9999999)}
+    with_options numericality: { other_than: 1, message: "can't be blank"} do
+      validates :category_id
+      validates :condition_id
+      validates :charge_id
+      validates :area_id
+      validates :day_id
+    end
   end
 
   belongs_to :user
   # has_one :history
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :charge
+  belongs_to :area
+  belongs_to :day
 
 end
