c4624964ddcd0154d6fd8d1ba60dad094315f2fc
KUMA-06 <hyt_kmkr@icloud.com>
2021-07-20 15:02:57 +0900

    編集画面　修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :item_find, only: [:show, :edit, :update]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -19,16 +20,14 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-    redirect_to root_path unless current_user.id == @item.user_id
+    redirect_check
   end
 
   def update
-    @item = Item.find(params[:id])
+    redirect_check
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
@@ -41,5 +40,13 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :item_name, :description, :category_id, :condition_id, :charge_id, :area_id, :day_id,
                                  :price).merge(user_id: current_user.id)
+  end
+
+  def item_find
+    @item = Item.find(params[:id])
+  end
+
+  def redirect_check
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 end
