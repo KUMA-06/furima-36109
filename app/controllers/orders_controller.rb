96ee205486d31667dd5ae9c0978edb01192bb7e6
KUMA-06 <hyt_kmkr@icloud.com>
2021-07-21 18:40:21 +0900

    バリデーション修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,13 +6,19 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @item = Item.find(params[:id])
+    @item = Item.find(params[:item_id])
     @history_buy = HistoryBuy.new(history_buy_params)
+    if @history_buy.valid?
+      @history_buy.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
   private
 
   def history_buy_params
-    params.require(:history_buy).permit(:postal, :area_id, :municipality, :address, :building, :phone).merge(user_id: current_user.id, item_id: params[:item_id], history_id: params[:history_id], buy_id: params[:buy_id])
+    params.require(:history_buy).permit(:postal, :area_id, :municipality, :address, :building, :phone).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
--- app/models/history_buy.rb
+++ app/models/history_buy.rb
@@ -1,21 +1,19 @@
 class HistoryBuy
   include ActiveModel::Model
-  attr_accessor :postal, :area_id, :municipality, :address, :building, :phone, :history_id, :user_id, :item_id, :buy_id
+  attr_accessor :postal, :area_id, :municipality, :address, :building, :phone, :user_id, :item_id
 
   with_options presence: true do
     validates :postal, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :municipality
     validates :address
-    validates :phone, format:(/\d{10-11}/)
-    validates :history_id
+    validates :phone, format:(/\A\d{10,11}\z/)
     validates :user_id
     validates :item_id
-    validates :buy_id
   end
 
   def save
-    history = history.create(user_id: user_id, item_id: item_id)
-    buy.create(postal: postal, area_id: area_id, municipality: municipality, address: address, phone: phone, history_id: history.id)
+    history = History.create(user_id: user_id, item_id: item_id)
+    Buy.create(postal: postal, area_id: area_id, municipality: municipality, address: address, building: building, phone: phone, history_id: history.id)
   end
 end
